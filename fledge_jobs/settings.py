eaa12a49ccbccf0f45fc1513f4acf386b7f7d77c
riaurko <riaurko@gmail.com>
2025-04-26 22:17:48 +0600

    Fixed 400 in Deployment

--- fledge_jobs/settings.py
+++ fledge_jobs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b&7-st%mw@5m2^vi7zt-n63^u!iayxy9mgv&!9rk0ose9^7!y5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['fledgejobs.vercel.app', '127.0.0.1']
 
 AUTH_USER_MODEL = 'user.User'
 
